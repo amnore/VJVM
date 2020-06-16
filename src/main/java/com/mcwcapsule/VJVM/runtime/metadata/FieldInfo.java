29517a58b8736a2d5a366f4bcd2bd34c8a9c2251
MrChenWithCapsule <Mr.ChenWithCapsule@outlook.com>
2020-06-16 16:02:47 +0800

    add fields and methods

--- src/main/java/com/mcwcapsule/VJVM/runtime/metadata/FieldInfo.java
+++ src/main/java/com/mcwcapsule/VJVM/runtime/metadata/FieldInfo.java
@@ -1,5 +1,68 @@
 package com.mcwcapsule.VJVM.runtime.metadata;
 
+import java.io.DataInput;
+import java.io.IOException;
+
+import com.mcwcapsule.VJVM.runtime.metadata.attribute.Attribute;
+import com.mcwcapsule.VJVM.runtime.metadata.constant.UTF8Constant;
+
+import lombok.Getter;
+
+import static com.mcwcapsule.VJVM.runtime.metadata.FieldAccessFlags.*;
+
 public class FieldInfo {
-    
+    private final short accessFlags;
+    @Getter
+    private final String name;
+    @Getter
+    private final String descriptor;
+    private final Attribute[] attributes;
+
+    public FieldInfo(DataInput dataInput, RuntimeConstantPool constantPool) {
+        try {
+            accessFlags = dataInput.readShort();
+            int nameIndex = dataInput.readUnsignedShort();
+            name = ((UTF8Constant) constantPool.getConstant(nameIndex)).getValue();
+            int descIndex = dataInput.readUnsignedShort();
+            descriptor = ((UTF8Constant) constantPool.getConstant(descIndex)).getValue();
+            int attributesCount = dataInput.readUnsignedShort();
+            attributes = new Attribute[attributesCount];
+            for (int i = 0; i < attributesCount; ++i)
+                attributes[i] = Attribute.constructFromData(dataInput, constantPool);
+        } catch (IOException e) {
+            throw new ClassFormatError();
+        }
+    }
+
+    public boolean isPublic() {
+        return (accessFlags & ACC_PUBLIC) != 0;
+    }
+
+    public boolean isPrivate() {
+        return (accessFlags & ACC_PRIVATE) != 0;
+    }
+
+    public boolean isProtected() {
+        return (accessFlags & ACC_PROTECTED) != 0;
+    }
+
+    public boolean isStatic() {
+        return (accessFlags & ACC_STATIC) != 0;
+    }
+
+    public boolean isFinal() {
+        return (accessFlags & ACC_FINAL) != 0;
+    }
+
+    public boolean isTransient() {
+        return (accessFlags & ACC_TRANSIENT) != 0;
+    }
+
+    public boolean isSynthetic() {
+        return (accessFlags & ACC_SYNTHETIC) != 0;
+    }
+
+    public boolean isEnum() {
+        return (accessFlags & ACC_ENUM) != 0;
+    }
 }
--- src/main/java/com/mcwcapsule/VJVM/runtime/metadata/InterfaceMethodInfo.java
+++ src/main/java/com/mcwcapsule/VJVM/runtime/metadata/InterfaceMethodInfo.java
@@ -1,5 +1,84 @@
 package com.mcwcapsule.VJVM.runtime.metadata;
 
-public class InterfaceMethodInfo {
+import java.io.DataInput;
+import java.io.IOException;
 
+import com.mcwcapsule.VJVM.runtime.metadata.attribute.Attribute;
+import com.mcwcapsule.VJVM.runtime.metadata.constant.UTF8Constant;
+
+import static com.mcwcapsule.VJVM.runtime.metadata.MethodAccessFlags.*;
+
+import lombok.Getter;
+
+public class InterfaceMethodInfo {
+    private short accessFlags;
+    @Getter
+    private String name;
+    @Getter
+    private String descriptor;
+    private Attribute[] attributes;
+
+    public InterfaceMethodInfo(DataInput dataInput, RuntimeConstantPool constantPool) {
+        try {
+            accessFlags = dataInput.readShort();
+            int nameIndex = dataInput.readUnsignedShort();
+            name = ((UTF8Constant) constantPool.getConstant(nameIndex)).getValue();
+            int descriptorIndex = dataInput.readUnsignedShort();
+            descriptor = ((UTF8Constant) constantPool.getConstant(descriptorIndex)).getValue();
+            int attrCount = dataInput.readUnsignedShort();
+            attributes = new Attribute[attrCount];
+            for (int i = 0; i < attrCount; ++i)
+                attributes[i] = Attribute.constructFromData(dataInput, constantPool);
+        } catch (IOException e) {
+            throw new ClassFormatError();
+        }
+    }
+
+    public boolean isPublic() {
+        return (accessFlags & ACC_PUBLIC) != 0;
+    }
+
+    public boolean isPrivate() {
+        return (accessFlags & ACC_PRIVATE) != 0;
+    }
+
+    public boolean isProtected() {
+        return (accessFlags & ACC_PROTECTED) != 0;
+    }
+
+    public boolean isStatic() {
+        return (accessFlags & ACC_STATIC) != 0;
+    }
+
+    public boolean isFinal() {
+        return (accessFlags & ACC_FINAL) != 0;
+    }
+
+    public boolean isSynchronized() {
+        return (accessFlags & ACC_SYNCHRONIZED) != 0;
+    }
+
+    public boolean isBridge() {
+        return (accessFlags & ACC_BRIDGE) != 0;
+    }
+
+    public boolean isVaargs() {
+        return (accessFlags & ACC_VARARGS) != 0;
+    }
+
+    public boolean isNative() {
+        return (accessFlags & ACC_NATIVE) != 0;
+    }
+
+    public boolean isAbstract() {
+        return (accessFlags & ACC_ABSTRACT) != 0;
+    }
+
+    public boolean isStrict() {
+        return (accessFlags & ACC_STRICT) != 0;
+    }
+
+    public boolean isSynthetic() {
+        return (accessFlags & ACC_SYNTHETIC) != 0;
+    }
 }
