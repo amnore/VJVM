474a6658d3270d0699e4c3f4f1c5d3e2459656cd
MrChenWithCapsule <Mr.ChenWithCapsule@outlook.com>
2020-06-21 17:29:33 +0800

    add vmoptions

--- src/main/java/com/mcwcapsule/VJVM/vm/VMOptions.java
+++ src/main/java/com/mcwcapsule/VJVM/vm/VMOptions.java
@@ -1,8 +1,17 @@
 package com.mcwcapsule.VJVM.vm;
 
 import lombok.Builder;
+import lombok.Getter;
 
 @Builder
+@Getter
 public class VMOptions {
-
+    @Builder.Default
+    private final String bootstrapClassPath = "";
+    @Builder.Default
+    private final String userClassPath = ".";
+    @Builder.Default
+    private final int heapSize = 1024;
+    private final String entryClass;
+    private final String[] args;
 }
