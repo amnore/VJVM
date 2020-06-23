51a5fd026ae9aba7e86c10ae1618d2b5345d0b03
MrChenWithCapsule <Mr.ChenWithCapsule@outlook.com>
2020-06-23 21:53:19 +0800

    add math test

--- src/test/java/com/mcwcapsule/VJVM/RunTest1.java
+++ src/test/java/com/mcwcapsule/VJVM/RunTest1.java
@@ -1,4 +1,17 @@
 package com.mcwcapsule.VJVM;
 
+import com.mcwcapsule.VJVM.utils.FileUtil;
+import com.mcwcapsule.VJVM.vm.VJVM;
+import com.mcwcapsule.VJVM.vm.VMOptions;
+import org.junit.jupiter.api.Test;
+
+import java.nio.file.Path;
+
 public class RunTest1 {
+    @Test
+    void test() {
+        Path path = CompileUtil.compile("Test5.java", "TestUtil.java");
+        VJVM.init(VMOptions.builder().entryClass("testsource/Test5").userClassPath(path.toString()).bootstrapClassPath("lib").build());
+        FileUtil.DeleteRecursive(path.toFile());
+    }
 }
