0adf95e5b705d0140f385aa8c5d09bab30d24b67
MrChenWithCapsule <Mr.ChenWithCapsule@outlook.com>
2020-06-24 22:25:35 +0800

    fix test

--- src/test/java/com/mcwcapsule/VJVM/CLITest.java
+++ src/test/java/com/mcwcapsule/VJVM/CLITest.java
@@ -1,33 +1,39 @@
 package com.mcwcapsule.VJVM;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintStream;
-import java.nio.ByteBuffer;
-
 import com.mcwcapsule.VJVM.cli.CLI;
 import com.mcwcapsule.VJVM.utils.ByteBufferInputStream;
 import com.mcwcapsule.VJVM.utils.ByteBufferOutputStream;
-
+import lombok.var;
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import lombok.var;
+import java.io.*;
+import java.nio.ByteBuffer;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 /**
  * Unit test for CLI.
  */
 class CLITest {
-    ByteBuffer inBuf = ByteBuffer.allocate(1024);
-    ByteBuffer outBuf = ByteBuffer.allocate(1024);
-    ByteBuffer errBuf = ByteBuffer.allocate(1024);
+    static ByteBuffer inBuf = ByteBuffer.allocate(1024);
+    static ByteBuffer outBuf = ByteBuffer.allocate(1024);
+    static ByteBuffer errBuf = ByteBuffer.allocate(1024);
+
+    @AfterAll
+    static void cleanup() {
+        System.setIn(new FileInputStream(FileDescriptor.in));
+        System.setOut(new PrintStream(new FileOutputStream(FileDescriptor.out)));
+        System.setErr(new PrintStream(new FileOutputStream(FileDescriptor.err)));
+    }
 
     @BeforeEach
     void setupStdio() {
+        inBuf.clear();
+        outBuf.clear();
+        errBuf.clear();
         System.setIn(new ByteBufferInputStream(inBuf));
         System.setOut(new PrintStream(new ByteBufferOutputStream(outBuf)));
         System.setErr(new PrintStream(new ByteBufferOutputStream(errBuf)));
@@ -35,7 +41,7 @@ class CLITest {
 
     @Test
     void testHelp() {
-        var args = new String[] { "-h", "-cp", "/lib", "VJVM" };
+        var args = new String[]{"-h", "-cp", "/lib", "VJVM"};
         CLI.main(args);
         try (BufferedReader reader = new BufferedReader(new InputStreamReader(new ByteBufferInputStream(outBuf)))) {
             assertTrue(reader.readLine().startsWith("usage"));
@@ -46,7 +52,7 @@ class CLITest {
 
     @Test
     void testEmpty() {
-        var args = new String[] {};
+        var args = new String[]{};
         CLI.main(args);
         try (BufferedReader reader = new BufferedReader(new InputStreamReader(new ByteBufferInputStream(outBuf)))) {
             assertTrue(reader.readLine().startsWith("usage"));
@@ -57,7 +63,7 @@ class CLITest {
 
     @Test
     void testMissingClass() {
-        var args = new String[] { "-cp", "/usr/" };
+        var args = new String[]{"-cp", "/usr/"};
         CLI.main(args);
         try (BufferedReader reader = new BufferedReader(new InputStreamReader(new ByteBufferInputStream(errBuf)))) {
             assertEquals("Main class required.", reader.readLine());
