cc0cc2ffedf80283cf650001aa750e64a7aa5b25
MrChenWithCapsule <Mr.ChenWithCapsule@outlook.com>
2020-07-03 18:55:21 +0800

    try fix bootstrap class path #2

--- 275-jvm-test/src/main/java/com/mcwcapsule/VJVM/utils/ClassPathUtil.java
+++ 275-jvm-test/src/main/java/com/mcwcapsule/VJVM/utils/ClassPathUtil.java
@@ -2,8 +2,6 @@ package com.mcwcapsule.VJVM.utils;
 
 import lombok.val;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.util.regex.Pattern;
 
 public class ClassPathUtil {
@@ -12,24 +10,6 @@ public class ClassPathUtil {
         val m = p.matcher(System.getProperty("java.version"));
         assert m.find();
         assert Double.parseDouble(m.group(1)) <= 1.81;
-
-        // find java path
-        // Oracle jdk not tested
-        // only linux supported
-        try {
-            val r = Runtime.getRuntime();
-            val po = r.exec("whereis java");
-            assert po.waitFor() == 0;
-            val reader = new BufferedReader(new InputStreamReader(po.getInputStream()));
-            val pathp = Pattern.compile("((?:\\w|-|/)+jvm(?:\\w|-|/)+)/bin/java");
-            val l = reader.readLine();
-            val pathm = pathp.matcher(l);
-            if (!pathm.find())
-                throw new Error(l);
-            System.err.println(pathm.group(1));
-            return pathm.group(1) + "/jre/lib/rt.jar";
-        } catch (Exception e) {
-            throw new Error(e);
-        }
+        return System.getProperty("sun.boot.class.path");
     }
 }
