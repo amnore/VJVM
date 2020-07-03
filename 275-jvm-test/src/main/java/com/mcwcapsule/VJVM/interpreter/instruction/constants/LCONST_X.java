c8d2f06e4a664b8e9f7d6c98ba9d80a5b79d0f00
MrChenWithCapsule <Mr.ChenWithCapsule@outlook.com>
2020-07-03 21:33:20 +0800

    fix lconst_x

--- 275-jvm-test/src/main/java/com/mcwcapsule/VJVM/interpreter/instruction/constants/LCONST_X.java
+++ 275-jvm-test/src/main/java/com/mcwcapsule/VJVM/interpreter/instruction/constants/LCONST_X.java
@@ -10,7 +10,7 @@ public class LCONST_X extends Instruction {
 
     @Override
     public void fetchAndRun(JThread thread) {
-        thread.getCurrentFrame().getOpStack().pushFloat(value);
+        thread.getCurrentFrame().getOpStack().pushLong(value);
     }
 
 }
