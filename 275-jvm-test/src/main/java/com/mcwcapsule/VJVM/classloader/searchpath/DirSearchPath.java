339a4e4fb0a205600e97eea8beed45223723dda4
MrChenWithCapsule <Mr.ChenWithCapsule@outlook.com>
2020-07-07 01:29:01 +0800

    fix search path assertion

--- 275-jvm-test/src/main/java/com/mcwcapsule/VJVM/classloader/searchpath/DirSearchPath.java
+++ 275-jvm-test/src/main/java/com/mcwcapsule/VJVM/classloader/searchpath/DirSearchPath.java
@@ -11,7 +11,7 @@ public class DirSearchPath extends ClassSearchPath {
 
     public DirSearchPath(String path) {
         searchPath = FileSystems.getDefault().getPath(path);
-        assert searchPath.toFile().isDirectory();
+        assert !searchPath.toFile().exists() || searchPath.toFile().isDirectory();
     }
 
     @Override
