92712deacaf2edb1eb8c54b79d4d9c812ec9c0d8
Martin77 <1754350460@qq.com>
2022-02-16 10:35:30 +0800

    update some todo

--- README.md
+++ README.md
@@ -6,3 +6,7 @@ VJVM: Virtual Java Virtual Machine (toy)
 1. 加了一些必要方法的javadoc
 2. 将一些Java高级特性（如java stream）修改成了大一同学更能看得懂的表示
 3. 修改了部分个人觉得不太好的面向对象
+
+## TODO
+1. 指令的细化 和19级一样分类型 将fetchOperand和execute分开
+
--- src/main/java/vjvm/interpreter/instruction/Instruction.java
+++ src/main/java/vjvm/interpreter/instruction/Instruction.java
@@ -3,5 +3,6 @@ package vjvm.interpreter.instruction;
 import vjvm.runtime.JThread;
 
 public abstract class Instruction {
-  public abstract void fetchAndRun(JThread thread);
+
+	public abstract void fetchAndRun(JThread thread);
 }
