f8e19fe9633f3ea7fbda6e6c5b0f98c3a5646459
Yihui Wang <191250142@smail.nju.edu.cn>
2022-04-12 16:32:05 +0800

    test: add LoadedTest

--- src/test/java/lab1/FindClasses.java
+++ src/test/java/lab1/FindClasses.java
@@ -1,10 +1,10 @@
 package lab1;
 
-import lombok.var;
 import org.junit.jupiter.api.Test;
 import picocli.CommandLine;
 import vjvm.vm.Main;
 
+import java.io.File;
 import java.nio.file.FileSystems;
 import java.nio.file.Path;
 import java.util.ArrayList;
@@ -13,6 +13,7 @@ import java.util.function.Function;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static lab1.Utils.runCmd;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class FindClasses {
   final Path resPath = FileSystems.getDefault().getPath(System.getenv("VJVM_TESTRES_PATH"));
@@ -45,5 +46,15 @@ class FindClasses {
 
     assertEquals(0, exec.apply("java.lang.String"));
     assertNotEquals(0, exec.apply("java.lang.None1234"));
+  }
+
+  @Test
+  void findInDirTwice() {
+    Function<String, Integer> exec = (c) -> runCmd(resPath.toString(), "lab1.cases." + c);
+
+    assertEquals(0, exec.apply("LoadedTest"));
+    File file = new File(resPath.toString(), "lab1.cases.LoadedTest".replace(".", File.separator) + ".class");
+    assertTrue(file.delete());
+    assertEquals(0, exec.apply("LoadedTest"));
   }
 }
